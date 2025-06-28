a66b854cb20b823a2a303062fe541e963b88a08e
Shivam Jha <shivamkumar87148@gmail.com>
2025-06-28 23:04:55 +0530

    Minor fixes
    Signed-off-by: Shivam Jha <shivamkumar87148@gmail.com>

--- cmd/scan.go
+++ cmd/scan.go
@@ -12,7 +12,7 @@ var outputPath string
 var scanCmd = &cobra.Command{
 	Use:   "scan [image]",
 	Short: "Scan a Docker image for vulnerabilities",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.ExactArgs(1),	
 	Run: func(cmd *cobra.Command, args []string) {
 		image := args[0]
 		report, err := scanner.RunScan(image)
